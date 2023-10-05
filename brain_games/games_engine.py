38306ab976e24c8c75483b74a7406da6bd6027cd
Abra19 <evabrarova@gmail.com>
2023-10-05 18:28:41 +0300

    fix long incorrect answer

--- brain_games/games_engine.py
+++ brain_games/games_engine.py
@@ -15,11 +15,11 @@ def launch_game(game):
         print(f'Question: {question}')
         your_answer = prompt.string('Your answer: ')
 
-        incorrect_1 = f"'{your_answer}' is wrong answer ;(. Correct answer "
-        incorrect_2 = f"was '{answer}'.\nLet's try again, {gamer_name}!"
+        incorrect = f"'{your_answer}' is wrong answer ;(. " \
+            f"Correct answer was '{answer}'.\nLet's try again, {gamer_name}!"
 
         if your_answer != answer:
-            print(incorrect_1 + incorrect_2)
+            print(incorrect)
             return
         print('Correct!')
 
